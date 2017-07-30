113a8b13449f2ded53b6e0a7e166e2534d7fb1f8
Kurtis Best <dacobra3@hotmail.com>
2017-07-30 13:29:34 -0400

    added main to package link to index.js

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+//value objects
+export * from './model/value-objects/common/address';
+export * from './model/value-objects/common/contact-attributes';
+export * from './model/value-objects/common/contact-name';
+export * from './model/value-objects/common/enumeration';
+export * from './model/value-objects/common/fee-item';
+export * from './model/value-objects/common/timed-result';
+
+export * from './model/value-objects/enumerators/filetype';
+
+//entities
+export * from './model/entities/entity-model-type';
+export * from './model/entities/category/category';
+export * from './model/entities/security/group';
+export * from './model/entities/security/resource';
+export * from './model/entities/security/user-token';
+export * from './model/entities/security/user';
