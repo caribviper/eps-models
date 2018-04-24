67558be542cca57dfb3d1f95f3b0604605f82c83
Kurtis Best <dacobra3@hotmail.com>
2018-04-23 21:18:28 -0400

    feat: Added documentation and new types like owner, occupier, noticee and ignore

--- src/model/value-objects/common/stakeholder.ts
+++ src/model/value-objects/common/stakeholder.ts
@@ -2,12 +2,27 @@ import { Contact } from './contact';
 
 /**Types of stakeholders */
 export const STAKEHOLDER_TYPES = {
+  //Applicant
   APPLICANT: 'applicant',
+  //Secondary applicant
   APPLICANT_SECONDARY: 'applicant secondary',
+  //Agent acting on applicant(s) behalf
   AGENT: 'agent',
+  //Person making complaint
   COMPLAINANT: 'complainant',
+  //Person committing alleged offence
   OFFENDER: 'offender',
+  //Person committing alleged offence
+  OWNER: 'owner',
+  //Person committing alleged offence
+  OCCUPIER: 'occupier',
+  //Person receiving notice
+  NOTICEE: 'notice recipient',
+  //Any other third party recipient
   THIRD_PARTY: 'third party',
+  //Ignores the specified contact
+  IGNORE: 'ignore contact',
+  //An associated organisation
   ASSOCIATED_ORGANISATION: 'associated organisation'
 };
 
@@ -15,6 +30,9 @@ export const STAKEHOLDER_TYPES = {
 export class Stakeholder {
   /**Indicates if the stakeholder is active. A false indicates that the stakeholder was present but is no longer active */
   active: boolean;
+
+  /**Possible secondary type to assist with notices and other areas that require two categories for stakeholder */
+  secondaryType: string = '';
 
   /**
    * Creates a new stakeholder
