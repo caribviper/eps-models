6a7a0cb3c0a9973db0f99c17f5fde309141119be
Kurtis Best <dacobra3@hotmail.com>
2018-04-23 21:17:09 -0400

    feat: added infractions and infraction period to invest.

--- src/model/entities/planning/enforcement/invest.ts
+++ src/model/entities/planning/enforcement/invest.ts
@@ -2,9 +2,17 @@ import { IRegistryDetails, RegistryDetails } from './../iregistry-details';
 
 export class Invest extends RegistryDetails {
 
-  comments: string = '';
-  directionsToSite: string = '';
-  mainInvestigation: string = '';
-  offendingAction: string = '';
-  
+  public comments: string = '';
+  public directionsToSite: string = '';
+  public mainInvestigation: string = '';
+  public offendingAction: string = '';
+
+  //the official enforcable item
+  public infraction: string = '';
+
+  //Gets or sets the start date of the infraction
+  public infractionStartDate: Date = null;
+
+  //Gets or sets infraction end date
+  public infractionEndDate: Date = null;
 }
