5083bd4bc7e80daa67b597df44429f09cf3cce5e
Kurtis Best <dacobra3@hotmail.com>
2018-06-14 20:26:04 -0400

    feat: has new isCompany property added fullnameWithTitleCapitalise property feat: added static methods isEmpty and isContactCompany fix: clone and getFullname methods

--- src/model/value-objects/common/contact.ts
+++ src/model/value-objects/common/contact.ts
@@ -1,3 +1,4 @@
+import { StringUtilities } from 'caribviper-common';
 import { Address } from './address';
 
 export const OrganisationTitleValues: string[] = ['COMPANY', 'BARBADOS ASSOCIATION', 'THE GENERAL MANAGER', 'THE MANAGER', 'THE REGISTRAR'];
@@ -24,6 +25,11 @@ export class Contact {
     return (!this.address || this.address.isEmpty) && !this.company && !this.lastname;
   }
 
+  /**Gets whether the contact is a company */
+  public get isCompany(): boolean {
+    return Contact.isContactCompany(this);
+  }
+
   /**
    * Gets the full name of the contact
    */
@@ -36,12 +42,25 @@ export class Contact {
     return Contact.getFullnameWithTitle(this);
   }
 
+  /**Get the full name of the contact as a lower case string */
+  public get fullnameWithTitleCapitalise(): string {
+    return StringUtilities.capitalize(Contact.getFullnameWithTitle(this));
+  }
+
   //Creates a new contact
   public clone(): Contact {
     return Contact.clone(this);
   }
 
+  public static isEmpty(c: Contact): boolean {
+    return (!c.address || c.address.isEmpty) && !c.company && !c.lastname;
+  }
+
   public static clone(contact: Contact): Contact {
+    if (!contact)
+      return null;
+    if (!contact.address)
+      return new Contact(null, contact.company, contact.firstname, contact.lastname, contact.title, contact.email, contact.telephone, contact.mobile);
     let a = new Address(contact.address.parish, contact.address.streetOne, contact.address.streetTwo, contact.address.lot, contact.address.country, contact.address.postalCode);
     a.inCareOf = contact.address.inCareOf;
     let c = new Contact(a, contact.company, contact.firstname, contact.lastname, contact.title, contact.email, contact.telephone, contact.mobile);
@@ -50,7 +69,8 @@ export class Contact {
 
   public static getFullname(c: Contact): string {
     let f: string = '';
-    if (!!c.company)
+    let fullnameAlias = StringUtilities.replaceAll(`${c.firstname} ${c.middleName} ${c.lastname}`, '  ', ' ');
+    if (!!c.company && c.company !== fullnameAlias)
       f = c.company;
     if (!!c.firstname) {
       f = (f.length > 0) ? f.concat(', ', c.firstname) : c.firstname;
@@ -94,6 +114,10 @@ export class Contact {
         f = (f.length > 0) ? f.concat(' ', contact.lastname) : contact.lastname;
     }
     return f;
+  }
+
+  public static isContactCompany(contact: Contact): boolean {
+    return contact.fullname !== contact.company && !!contact.company;
   }
 }
 
