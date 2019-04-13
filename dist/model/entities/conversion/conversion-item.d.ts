2be597beacd31c72c8e391eaadfde07c457e6eb3
caribviper <dacobra3@hotmail.com>
2019-04-13 02:30:50 -0400

    chore: removed old code

--- dist/model/entities/conversion/conversion-item.d.ts
+++
@@ -1,9 +0,0 @@
-import { Entity } from 'caribviper-entity';
-export declare class ConvertionItem extends Entity {
-    lastUpdate: Date;
-    constructor(lastUpdate?: Date);
-    validateEntity(): void;
-    static createId(): string;
-    static mapToEntity(source: any): ConvertionItem;
-    static mapToEntityArray(source: ConvertionItem[]): ConvertionItem[];
-}
--- dist/model/value-objects/common/organisation-info.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class OrganisationInfo {
-    readonly orgId: string;
-    name: string;
-    constructor(orgId: string, name: string);
-}
